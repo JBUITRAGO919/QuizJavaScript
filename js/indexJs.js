d4babec0d7e23e277fbbdf41c2d7b4542e7be5bf
jbuitrago <jbuitrago919@misena.edu.co>
2020-12-04 22:49:50 -0500

    se sube el segundo ejrcicio

--- js/indexJs.js
+++ js/indexJs.js
@@ -1,6 +1,4 @@
-
-
-  function tipoMotor(motor){
+ function tipoMotor(motor){
         
        
         switch (motor) {
@@ -33,3 +31,28 @@
 tipoMotor(parseInt(prompt("digite el valor")));
 
 
+function obtenervalorConImpuesto(tipoProducto, valorSinImpuesto){
+    var valorSinImpuesto;
+    var tipoProducto;
+    
+    switch (tipoProducto) {
+        case 1:
+            valor =valorSinImpuesto*21/100;
+        alert("El valor del producto de tipo 1 mas el 21% es :"+ valor)    
+            break;
+        case 2:
+            valor =valorSinImpuesto*10/100;
+        alert("El valor del producto de tipo 2 mas el 10% es :"+ valor)
+
+        case 3:
+            valor = valorSinImpuesto*5/100;
+        alert("El valor del producto de tipo 2 mas el 10% es :"+ valor)
+    
+        default:
+            alert("No es un tipo valido")   
+            break;
+    }
+    };
+    obtenervalorConImpuesto(3, 100)
+
+    
